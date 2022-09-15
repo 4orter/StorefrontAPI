744092f0c9fb25584fb44887d103bcdea5d5817f
Tye Porter <tyeporterapps@gmail.com>
2022-09-14 23:08:02 -0500

    Updates database

--- src/frameworks/databases/mock/MockDatabase.ts
+++ src/frameworks/databases/mock/MockDatabase.ts
@@ -23,28 +23,43 @@ const MockDatabase: {
     userSessions: [],
     productCategories: [],
     async initialize(): Promise<void> {
-        jsf.extend('chance', () => new Chance());
-        const json = JSON.stringify(jsf.generate(MockSchema));
+        const filePath = path.join(__dirname, './data.json');
 
-        fs.writeFile(path.join(__dirname, './data.json'), json, (err: Error | null) => {
-            if (err) {
-                console.log(chalk.red(err));
-                return Promise.reject(err);
-            } else {
-                console.log(chalk.green('Mock data generated.'));
-                fs.readFile(path.join(__dirname, './data.json'), {encoding: 'utf-8'}, (err: Error | null, data: string) => {
-                    if (err) {
-                        console.log(chalk.red(err));
-                        return Promise.reject(err);
-                    } else {
-                        this.users = JSON.parse(data).users as User[];
-                        this.products = JSON.parse(data).products as Product[];
-                        console.log(chalk.green('In-memory database initialized.'));
-                        return Promise.resolve();
-                    }
-                });
-            }
-        });
+        if (fs.existsSync(filePath)) {
+            fs.readFile(filePath, {encoding: 'utf-8'}, (err: Error | null, data: string) => {
+                if (err) {
+                    console.log(chalk.red(err));
+                    return Promise.reject(err);
+                } else {
+                    this.users = JSON.parse(data).users as User[];
+                    this.products = JSON.parse(data).products as Product[];
+                    console.log(chalk.green('In-memory database initialized.'));
+                    return Promise.resolve();
+                }
+            });
+        } else {
+            jsf.extend('chance', () => new Chance());
+            const json = JSON.stringify(jsf.generate(MockSchema));
+            fs.writeFile(filePath, json, (err: Error | null) => {
+                if (err) {
+                    console.log(chalk.red(err));
+                    return Promise.reject(err);
+                } else {
+                    console.log(chalk.green('Mock data generated.'));
+                    fs.readFile(filePath, {encoding: 'utf-8'}, (err: Error | null, data: string) => {
+                        if (err) {
+                            console.log(chalk.red(err));
+                            return Promise.reject(err);
+                        } else {
+                            this.users = JSON.parse(data).users as User[];
+                            this.products = JSON.parse(data).products as Product[];
+                            console.log(chalk.green('In-memory database initialized.'));
+                            return Promise.resolve();
+                        }
+                    });
+                }
+            });
+        }
     },
 };
 
